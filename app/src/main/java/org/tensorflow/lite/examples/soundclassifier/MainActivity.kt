949a397ecb9de4ed53aaa8032951489c303718e5
kris <kristinateatovawd@gmail.com>
2024-06-02 12:37:46 +0300

    Application for diploma project of bird sound classification

--- app/src/main/java/org/tensorflow/lite/examples/soundclassifier/MainActivity.kt
+++ app/src/main/java/org/tensorflow/lite/examples/soundclassifier/MainActivity.kt
@@ -24,8 +24,8 @@ class MainActivity : AppCompatActivity() {
   private val probabilitiesAdapter by lazy { ProbabilitiesAdapter() }
   private var audioClassifier: AudioClassifier? = null
   private var audioRecord: AudioRecord? = null
-  private var classificationInterval = 500L // how often should classification run in milli-secs
-  private lateinit var handler: Handler // background thread handler to run classification
+  private var classificationInterval = 500L // как часто классификация будет срабатывать
+  private lateinit var handler: Handler
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -39,16 +39,9 @@ class MainActivity : AppCompatActivity() {
         adapter = probabilitiesAdapter
       }
 
-      // Input switch to turn on/off classification
-    /*  keepScreenOn(inputSwitch.isChecked)
-      inputSwitch.setOnCheckedChangeListener { _, isChecked ->
-        if (isChecked) startAudioClassification() else stopAudioClassification()
-        keepScreenOn(isChecked)
-      }*/
-
       var isRecording = false
       class StatusHolder(var status: Boolean)
-// Создать кнопку
+      // Создать кнопку
      // Обработать нажатия кнопки
       myButton.setOnClickListener {
         // Изменить состояние записи
@@ -69,7 +62,7 @@ class MainActivity : AppCompatActivity() {
                   }
       }
 
-      // Slider which control how often the classification task should run
+      // Ползунок который отображает как часто срабатывает классификация
       classificationIntervalSlider.value = classificationInterval.toFloat()
       classificationIntervalSlider.setLabelFormatter { value: Float ->
         "${value.toInt()} ms"
@@ -83,12 +76,12 @@ class MainActivity : AppCompatActivity() {
 
 
 
-    // Create a handler to run classification in a background thread
+
     val handlerThread = HandlerThread("backgroundThread")
     handlerThread.start()
     handler = HandlerCompat.createAsync(handlerThread.looper)
 
-    // Request microphone permission and start running classification
+    // запрос разрешения на использование микрофона
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
       requestMicrophonePermission()
     } else {
@@ -97,27 +90,23 @@ class MainActivity : AppCompatActivity() {
   }
 
   private fun startAudioClassification() {
-    // If the audio classifier is initialized and running, do nothing.
-    if (audioClassifier != null) return;
+        if (audioClassifier != null) return;
 
     // Initialize the audio classifier
     val classifier = AudioClassifier.createFromFile(this, MODEL_FILE)
     val audioTensor = classifier.createInputTensorAudio()
 
-    // Initialize the audio recorder
+    // инициализация аудиорекордера
     val record = classifier.createAudioRecord()
     record.startRecording()
 
-    // Define the classification runnable
+
     val run = object : Runnable {
       override fun run() {
         val startTime = System.currentTimeMillis()
 
-        // Load the latest audio sample
         audioTensor.load(record)
         val output = classifier.classify(audioTensor)
-
-        // Filter out results above a certain threshold, and sort them descendingly
         val filteredModelOutput = output[0].categories.filter {
           it.score > MINIMUM_DISPLAY_THRESHOLD
         }.sortedBy {
@@ -127,22 +116,16 @@ class MainActivity : AppCompatActivity() {
         val finishTime = System.currentTimeMillis()
 
         Log.d(TAG, "Latency = ${finishTime - startTime}ms")
-
-        // Updating the UI
         runOnUiThread {
           probabilitiesAdapter.categoryList = filteredModelOutput
           probabilitiesAdapter.notifyDataSetChanged()
         }
-
-        // Rerun the classification after a certain interval
         handler.postDelayed(this, classificationInterval)
       }
     }
 
-    // Start the classification process
+    // запуск процесса классификации
     handler.post(run)
-
-    // Save the instances we just created for use later
     audioClassifier = classifier
     audioRecord = record
   }
